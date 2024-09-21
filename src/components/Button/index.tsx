1140cdb00921ead7404e1a78b42df191246395ae
mathieuz <matheusandrewww@gmail.com>
2024-09-21 04:59:55 -0300

    feat: definindo se um botão deve ou não ter o type=submit por prop

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,10 +3,14 @@ import styles from "./Button.module.css";
 
 interface ButtonProps {
     children?: ReactNode;
+    isSubmit?: boolean;
     onClick?: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({children, onClick}) => {
+const Button: React.FC<ButtonProps> = ({children, isSubmit, onClick}) => {
+
+    if (isSubmit) return <button className={styles.button} onClick={onClick} type="submit">{children}</button>
+    
     return(
         <button className={styles.button} onClick={onClick}>
             {children}
