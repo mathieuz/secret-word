623765e1a9fd08ed1f3fe37d61a4371c4e1f52c6
mathieuz <matheusandrewww@gmail.com>
2024-09-24 20:31:21 -0300

    feat: inclusão de novas palavras secretas no objeto secretWords

--- src/game/secretWords.ts
+++ src/game/secretWords.ts
@@ -8,7 +8,13 @@ const secretWords: ISecretWord[] = [
     { word: "BANANA", tip: "Fruta" },
     { word: "OUTONO", tip: "Estação do ano" },
     { word: "CHOCOLATE", tip: "Páscoa" },
-    { word: "MOTOCICLETA", tip: "Veículo" }
+    { word: "MOTOCICLETA", tip: "Veículo" },
+    { word: "INSTAGRAM", tip: "Rede Social" },
+    { word: "RELOGIO", tip: "Acessório" },
+    { word: "LASANHA", tip: "Comida" },
+    { word: "EUROPA", tip: "Continente" },
+    { word: "CHILE", tip: "País da américa do sul" },
+    { word: "SATURNO", tip: "Planeta" }
 ];
 
 export default secretWords;
